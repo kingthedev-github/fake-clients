8735aa22a6b324e67ae69298a21e425386440d18
King <gyattohio4444@gmail.com>
2024-08-10 01:01:52 -0400

    Clients V1

---
+++ Fake Clients.bat
@@ -0,0 +1,14 @@
+@echo off
+title Client Loader
+set /p clients="How Many Clients Would You Like: "
+cls
+
+title Clients: %clients%
+echo ****************************************
+echo Clients: %clients%
+echo ****************************************
+echo.
+echo.
+echo Managing Soon....
+echo.
+set /p exit="Press Enter To Exit"
